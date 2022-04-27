b5ad8e67b9ace837258413f956fffaee2c0027f5
sandek12@medtronic.com <26626@telxsids.net>
2022-04-27 12:42:22 +0530

    1st commit

---
+++ Scripts/Get/Script1651038988914.groovy
@@ -0,0 +1,18 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import java.nio.file.WatchService
+
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
+import groovy.json.JsonSlurper
+
+
+ResponseObject createPatientResponse = WS.sendRequest(findTestObject('Get_Request'))
+
+WS.verifyResponseStatusCode(createPatientResponse, 200)
+
+
+String ID = WS.getElementPropertyValue(createPatientResponse, 'support.url')
+
+
